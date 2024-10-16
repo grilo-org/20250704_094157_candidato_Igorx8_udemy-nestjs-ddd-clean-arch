58e668a5ce8f5fc62fceb38eb7e29c7b75d0866a
igorx8 <igor.carvalhox8@gmail.com>
2024-10-16 18:12:16 -0300

    creating user entity

--- src/shared/infrastructure/env-config/env-config.module.ts
+++ src/shared/infrastructure/env-config/env-config.module.ts
@@ -1,10 +1,14 @@
 import { DynamicModule, Module } from '@nestjs/common';
-import { ConfigModule, ConfigModuleOptions } from '@nestjs/config';
+import {
+  ConfigModule,
+  ConfigModuleOptions,
+  ConfigService,
+} from '@nestjs/config';
 import { join } from 'path';
 import { EnvConfigService } from './env-config.service';
 
 @Module({
-  providers: [EnvConfigService],
+  providers: [ConfigService, EnvConfigService],
 })
 export class EnvConfigModule extends ConfigModule {
   static forRoot(options: ConfigModuleOptions = {}): DynamicModule {
